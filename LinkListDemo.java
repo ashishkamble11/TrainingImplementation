fce42713074d2cea5cebf559165c7aea85ed1e43
ashishkamble11 <51822538+ashishkamble11@users.noreply.github.com>
2019-06-17 10:24:22 +0530

    Add files via upload

---
+++ LinkListDemo.java
@@ -0,0 +1,145 @@
+package com.company;
+
+public class LinkListDemo
+{
+    public static void main(String[] args)
+    {
+        List list=new List();
+
+        list.insert(list,1);
+        list.insert(list,2);
+        list.insert(list,3);
+        list.insert(list,4);
+        list.insert(list,5);
+
+        list.printList(list);
+
+        list.removeByKey(list,2);
+        list.removeByKey(list,6);
+
+        list.insertBegin(list,6);
+
+        list.printList(list);
+
+        list.insertAfter(list,4,7);
+
+        list.printList(list);
+    }
+}
+
+class List
+{
+    Node head;
+
+    static class Node
+    {
+        int key;
+        Node next;
+        Node(int d)
+        {
+            key=d;
+            next=null;
+        }
+    }
+
+    void printList(List list)
+    {
+        Node currnode=list.head;
+        System.out.println("Linkedlist: ");
+        while (currnode!=null)
+        {
+            System.out.println(currnode.key+" ");
+            currnode=currnode.next;
+        }
+        System.out.println();
+    }
+
+    List insert(List list, int data)
+    {
+        Node new_node=new Node(data);
+        new_node.next=null;
+
+        if(list.head==null)
+        {
+            list.head=new_node;
+        }
+        else
+        {
+            Node last=list.head;
+            while(last.next!=null)
+            {
+                last=last.next;
+            }
+            last.next=new_node;
+        }
+
+        return list;
+    }
+
+    List insertBegin(List list, int data)
+    {
+        Node newnode=new Node(data);
+        newnode.next=null;
+
+        if(list.head==null)
+        {
+            list.head=newnode;
+        }
+        else
+        {
+            newnode.next=head;
+            head=newnode;
+        }
+        return list;
+    }
+
+    List insertAfter(List list, int olddata, int newdata)
+    {
+        Node newnode=new Node(newdata);
+        Node currnode=list.head;
+
+        if(currnode!=null && currnode.key==olddata)
+        {
+            newnode.next=currnode.next;
+            list.head=newnode;
+        }
+        while(currnode!=null && currnode.key!=olddata)
+        {
+            currnode=currnode.next;
+        }
+        System.out.println("key is "+currnode.key);
+        newnode.next=currnode.next;
+        currnode.next=newnode;
+        return list;
+    }
+
+    List removeByKey(List list, int data)
+    {
+        Node currnode=list.head, prev=null;
+
+        if(currnode != null && currnode.key==data)
+        {
+            list.head=currnode.next;
+            System.out.println("Deleted "+data);
+            return list;
+        }
+
+        while (currnode!=null && currnode.key!=data)
+        {
+            prev=currnode;
+            currnode=currnode.next;
+        }
+
+        if(currnode!=null)
+        {
+            prev.next=currnode.next;
+            System.out.println("Deleted "+data);
+        }
+
+        if(currnode==null)
+        {
+            System.out.println("Data not found.");
+        }
+        return list;
+    }
+}
